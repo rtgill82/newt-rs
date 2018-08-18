7c3dcf3239ef5505e008510ae880d4023b2bacf8
Robert Gill <rtgill82@gmail.com>
2018-08-18 15:42:51 -0700

    Add Label tests

---
+++ tests/label_tests.rs
@@ -0,0 +1,38 @@
+extern crate newt;
+use newt::components::Component;
+use newt::components::Label;
+use newt::components::Form;
+use std::ptr;
+
+use newt::constants::COLORSET_LABEL;
+
+#[test]
+fn label_create() {
+    let label = Label::new(0, 0, "Ok");
+    assert!(label.co() != ptr::null());
+}
+
+#[test]
+fn label_partial_eq_true() {
+    let label = Label::new(0, 0, "Ok");
+    assert!(label == label);
+}
+
+#[test]
+fn label_partial_eq_false() {
+    let label = Label::new(0, 0, "Ok");
+    let form = Form::new(0);
+    assert!(label != form);
+}
+
+#[test]
+fn label_set_text() {
+    let label = Label::new(0, 0, "Ok");
+    label.set_text("Not Ok");
+}
+
+#[test]
+fn label_set_colors() {
+    let label = Label::new(0, 0, "Ok");
+    label.set_colors(COLORSET_LABEL);
+}
