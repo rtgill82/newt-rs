66df8efdbd491dce96d5c4ce0df3916cc2318298
Robert Gill <rtgill82@gmail.com>
2018-08-18 16:07:05 -0700

    Add Scale tests

---
+++ tests/scale_tests.rs
@@ -0,0 +1,39 @@
+extern crate newt;
+use newt::components::Component;
+use newt::components::Scale;
+use newt::components::Form;
+use std::ptr;
+
+use newt::constants::COLORSET_EMPTYSCALE;
+use newt::constants::COLORSET_FULLSCALE;
+
+#[test]
+fn scale_create() {
+    let scale = Scale::new(0, 0, 10, 100);
+    assert!(scale.co() != ptr::null());
+}
+
+#[test]
+fn scale_partial_eq_true() {
+    let scale = Scale::new(0, 0, 10, 100);
+    assert!(scale == scale);
+}
+
+#[test]
+fn scale_partial_eq_false() {
+    let scale = Scale::new(0, 0, 10, 100);
+    let form = Form::new(0);
+    assert!(scale != form);
+}
+
+#[test]
+fn scale_set() {
+    let scale = Scale::new(0, 0, 10, 100);
+    scale.set(50);
+}
+
+#[test]
+fn scale_set_colors() {
+    let scale = Scale::new(0, 0, 10, 100);
+    scale.set_colors(COLORSET_EMPTYSCALE, COLORSET_FULLSCALE);
+}
