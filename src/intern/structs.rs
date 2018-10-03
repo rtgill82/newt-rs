a8859c21ea81eb041f24e1362612a522d82740d3
Robert Gill <rtgill82@gmail.com>
2018-10-03 14:43:24 -0700

    Add set_colors and set_color functions

--- src/intern/structs.rs
+++ src/intern/structs.rs
@@ -1,3 +1,4 @@
+use std::os::raw::c_char;
 use std::os::raw::c_int;
 use components::c_component;
 
@@ -22,4 +23,31 @@ pub union ExitStructUnion {
 pub struct ExitStruct {
     pub reason: ExitStructEnum,
     pub u: ExitStructUnion
+}
+
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct NewtColors {
+    pub rootFg: *const c_char,          pub rootBg: *const c_char,
+    pub borderFg: *const c_char,        pub borderBg: *const c_char,
+    pub windowFg: *const c_char,        pub windowBg: *const c_char,
+    pub shadowFg: *const c_char,        pub shadowBg: *const c_char,
+    pub titleFg: *const c_char,         pub titleBg: *const c_char,
+    pub buttonFg: *const c_char,        pub buttonBg: *const c_char,
+    pub actButtonFg: *const c_char,     pub actButtonBg: *const c_char,
+    pub checkboxFg: *const c_char,      pub checkboxBg: *const c_char,
+    pub actCheckboxFg: *const c_char,   pub actCheckboxBg: *const c_char,
+    pub entryFg: *const c_char,         pub entryBg: *const c_char,
+    pub labelFg: *const c_char,         pub labelBg: *const c_char,
+    pub listboxFg: *const c_char,       pub listboxBg: *const c_char,
+    pub actListboxFg: *const c_char,    pub actListboxBg: *const c_char,
+    pub textboxFg: *const c_char,       pub textboxBg: *const c_char,
+    pub actTextboxFg: *const c_char,    pub actTextboxBg: *const c_char,
+    pub helpLineFg: *const c_char,      pub helpLineBg: *const c_char,
+    pub rootTextFg: *const c_char,      pub rootTextBg: *const c_char,
+    pub emptyScale: *const c_char,      pub fullScale: *const c_char,
+    pub disabledEntryFg: *const c_char, pub disabledEntryBg: *const c_char,
+    pub compactButtonFg: *const c_char, pub compactButtonBg: *const c_char,
+    pub actSelListboxFg: *const c_char, pub actSelListboxBg: *const c_char,
+    pub selListboxFg: *const c_char,    pub selListboxBg: *const c_char
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,33 @@ pub mod components;
 pub mod constants;
 pub use constants::*;
 
+use intern::structs::NewtColors;
+
+pub struct Colors<'a> {
+    pub root_fg: &'a str,            pub root_bg: &'a str,
+    pub border_fg: &'a str,          pub border_bg: &'a str,
+    pub window_fg: &'a str,          pub window_bg: &'a str,
+    pub shadow_fg: &'a str,          pub shadow_bg: &'a str,
+    pub title_fg: &'a str,           pub title_bg: &'a str,
+    pub button_fg: &'a str,          pub button_bg: &'a str,
+    pub act_button_fg: &'a str,      pub act_button_bg: &'a str,
+    pub checkbox_fg: &'a str,        pub checkbox_bg: &'a str,
+    pub act_checkbox_fg: &'a str,    pub act_checkbox_bg: &'a str,
+    pub entry_fg: &'a str,           pub entry_bg: &'a str,
+    pub label_fg: &'a str,           pub label_bg: &'a str,
+    pub listbox_fg: &'a str,         pub listbox_bg: &'a str,
+    pub act_listbox_fg: &'a str,     pub act_listbox_bg: &'a str,
+    pub textbox_fg: &'a str,         pub textbox_bg: &'a str,
+    pub act_textbox_fg: &'a str,     pub act_textbox_bg: &'a str,
+    pub help_line_fg: &'a str,       pub help_line_bg: &'a str,
+    pub root_text_fg: &'a str,       pub root_text_bg: &'a str,
+    pub empty_scale: &'a str,        pub full_scale: &'a str,
+    pub disabled_entry_fg: &'a str,  pub disabled_entry_bg: &'a str,
+    pub compact_button_fg: &'a str,  pub compact_button_bg: &'a str,
+    pub act_sel_listbox_fg: &'a str, pub act_sel_listbox_bg: &'a str,
+    pub sel_listbox_fg: &'a str,     pub sel_listbox_bg: &'a str
+}
+
 pub fn init() -> i32 {
     #[link(name="newt")]
     extern "C" { fn newtInit() -> c_int; }
@@ -109,6 +136,96 @@ pub fn pop_window_no_refresh() {
     extern "C" { fn newtPopWindowNoRefresh(); }
 
     unsafe { newtPopWindowNoRefresh(); }
+}
+
+pub fn set_colors(colors: &Colors) {
+    #[link(name="newt")]
+    extern "C" {
+        fn newtSetColors(colors: *const NewtColors);
+    }
+
+    let root_fg = CString::new(colors.root_fg).unwrap();
+    let root_bg = CString::new(colors.root_bg).unwrap();
+    let border_fg = CString::new(colors.border_fg).unwrap();
+    let border_bg = CString::new(colors.border_bg).unwrap();
+    let window_fg = CString::new(colors.window_fg).unwrap();
+    let window_bg = CString::new(colors.window_bg).unwrap();
+    let shadow_fg = CString::new(colors.shadow_fg).unwrap();
+    let shadow_bg = CString::new(colors.shadow_bg).unwrap();
+    let title_fg = CString::new(colors.title_fg).unwrap();
+    let title_bg = CString::new(colors.title_bg).unwrap();
+    let button_fg = CString::new(colors.button_fg).unwrap();
+    let button_bg = CString::new(colors.button_bg).unwrap();
+    let act_button_fg = CString::new(colors.act_button_fg).unwrap();
+    let act_button_bg = CString::new(colors.act_button_bg).unwrap();
+    let checkbox_fg = CString::new(colors.checkbox_fg).unwrap();
+    let checkbox_bg = CString::new(colors.checkbox_bg).unwrap();
+    let act_checkbox_fg = CString::new(colors.act_checkbox_fg).unwrap();
+    let act_checkbox_bg = CString::new(colors.act_checkbox_bg).unwrap();
+    let entry_fg = CString::new(colors.entry_fg).unwrap();
+    let entry_bg = CString::new(colors.entry_bg).unwrap();
+    let label_fg = CString::new(colors.label_fg).unwrap();
+    let label_bg = CString::new(colors.label_bg).unwrap();
+    let listbox_fg = CString::new(colors.listbox_fg).unwrap();
+    let listbox_bg = CString::new(colors.listbox_bg).unwrap();
+    let act_listbox_fg = CString::new(colors.act_listbox_fg).unwrap();
+    let act_listbox_bg = CString::new(colors.act_listbox_bg).unwrap();
+    let textbox_fg = CString::new(colors.textbox_fg).unwrap();
+    let textbox_bg = CString::new(colors.textbox_bg).unwrap();
+    let act_textbox_fg = CString::new(colors.act_textbox_fg).unwrap();
+    let act_textbox_bg = CString::new(colors.act_textbox_bg).unwrap();
+    let help_line_fg = CString::new(colors.help_line_fg).unwrap();
+    let help_line_bg = CString::new(colors.help_line_bg).unwrap();
+    let root_text_fg = CString::new(colors.root_text_fg).unwrap();
+    let root_text_bg = CString::new(colors.root_text_bg).unwrap();
+    let empty_scale = CString::new(colors.empty_scale).unwrap();
+    let full_scale = CString::new(colors.full_scale).unwrap();
+    let disabled_entry_fg = CString::new(colors.disabled_entry_fg).unwrap();
+    let disabled_entry_bg = CString::new(colors.disabled_entry_bg).unwrap();
+    let compact_button_fg = CString::new(colors.compact_button_fg).unwrap();
+    let compact_button_bg = CString::new(colors.compact_button_bg).unwrap();
+    let act_sel_listbox_fg = CString::new(colors.act_sel_listbox_fg).unwrap();
+    let act_sel_listbox_bg = CString::new(colors.act_sel_listbox_bg).unwrap();
+    let sel_listbox_fg = CString::new(colors.sel_listbox_fg).unwrap();
+    let sel_listbox_bg = CString::new(colors.sel_listbox_bg).unwrap();
+
+    let c_colors = NewtColors {
+        rootFg: root_fg.as_ptr(),                     rootBg: root_bg.as_ptr(),
+        borderFg: border_fg.as_ptr(),                 borderBg: border_bg.as_ptr(),
+        windowFg: window_fg.as_ptr(),                 windowBg: window_bg.as_ptr(),
+        shadowFg: shadow_fg.as_ptr(),                 shadowBg: shadow_bg.as_ptr(),
+        titleFg: title_fg.as_ptr(),                   titleBg: title_bg.as_ptr(),
+        buttonFg: button_fg.as_ptr(),                 buttonBg: button_bg.as_ptr(),
+        actButtonFg: act_button_fg.as_ptr(),          actButtonBg: act_button_bg.as_ptr(),
+        checkboxFg: checkbox_fg.as_ptr(),             checkboxBg: checkbox_bg.as_ptr(),
+        actCheckboxFg: act_checkbox_fg.as_ptr(),      actCheckboxBg: act_checkbox_bg.as_ptr(),
+        entryFg: entry_fg.as_ptr(),                   entryBg: entry_bg.as_ptr(),
+        labelFg: label_fg.as_ptr(),                   labelBg: label_bg.as_ptr(),
+        listboxFg: listbox_fg.as_ptr(),               listboxBg: listbox_bg.as_ptr(),
+        actListboxFg: act_listbox_fg.as_ptr(),        actListboxBg: act_listbox_bg.as_ptr(),
+        textboxFg: textbox_fg.as_ptr(),               textboxBg: textbox_bg.as_ptr(),
+        actTextboxFg: act_textbox_fg.as_ptr(),        actTextboxBg: act_textbox_bg.as_ptr(),
+        helpLineFg: help_line_fg.as_ptr(),            helpLineBg: help_line_bg.as_ptr(),
+        rootTextFg: root_text_fg.as_ptr(),            rootTextBg: root_text_bg.as_ptr(),
+        emptyScale: empty_scale.as_ptr(),             fullScale: full_scale.as_ptr(),
+        disabledEntryFg: disabled_entry_fg.as_ptr(),  disabledEntryBg: disabled_entry_bg.as_ptr(),
+        compactButtonFg: compact_button_fg.as_ptr(),  compactButtonBg: compact_button_bg.as_ptr(),
+        actSelListboxFg: act_sel_listbox_fg.as_ptr(), actSelListboxBg: act_sel_listbox_bg.as_ptr(),
+        selListboxFg: sel_listbox_fg.as_ptr(),        selListboxBg: sel_listbox_bg.as_ptr()
+    };
+
+    unsafe { newtSetColors(&c_colors); }
+}
+
+pub fn set_color(colorset: i32, fg: &str, bg: &str) {
+    #[link(name="newt")]
+    extern "C" {
+        fn newtSetColor(colorset: c_int, fg: *const c_char, bg: *const c_char);
+    }
+
+    let c_fg = CString::new(fg).unwrap();
+    let c_bg = CString::new(bg).unwrap();
+    unsafe { newtSetColor(colorset, c_fg.as_ptr(), c_bg.as_ptr()); }
 }
 
 pub fn refresh() {
