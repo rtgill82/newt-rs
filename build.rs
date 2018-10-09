78bb9669160fdb0f3039399f89b2d7b81b1965e1
Robert Gill <rtgill82@gmail.com>
2018-10-08 21:31:21 -0700

    Set required newt version to 0.52.15

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate pkg_config;
 
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("0.52.0")
+        .atleast_version("0.52.15")
         .probe("libnewt")
         .unwrap();
 }
