3eb682da2d7f126505b455f3b0f690da98c54172
Robert Gill <rtgill82@gmail.com>
2019-05-13 16:26:08 -0700

    Implement win_entries() for x86_64

--- examples/windows.rs
+++ examples/windows.rs
@@ -11,6 +11,26 @@ fn asm_functions() {
 
     win_message("Selection", "Ok",
                 &format!("Button selected: {}, Item selected: {}", rv, item));
+
+    let mut entries = Vec::new();
+    for n in 1..3 {
+        let entry: WinEntry = WinEntry::new(
+            &format!("Entry {}", n),
+            &format!("Value {}", n),
+            0);
+        entries.push(entry);
+    }
+
+    let rv = win_entries("Test Entries", "Message", 50, 5, 5, 10,
+                         entries.as_mut_slice(), &["Ok", "Cancel"]);
+
+    let mut message = String::new();
+    for (i, entry) in entries.iter().enumerate() {
+        message.push_str(&format!("Entry {}: {}, ", i + 1, entry.value()));
+    }
+
+    message.push_str(&format!("Button selected: {}", rv));
+    win_message("Entries", "Ok", &message);
 }
 
 pub fn main() {
--- src/windows.rs
+++ src/windows.rs
@@ -1,4 +1,7 @@
 use std::ffi::CString;
+use std::ffi::CStr;
+use std::mem::size_of;
+use std::os::raw::{c_char,c_void};
 use newt_sys::*;
 
 #[cfg(feature = "asm")]
@@ -191,4 +194,128 @@ pub fn win_menu(title: &str, text: &str, suggested_width: i32, flex_down: i32,
     }
 
     return (rv, list_item);
+}
+
+#[cfg(feature = "asm")]
+#[derive(Default)]
+pub struct WinEntry {
+    text: String,
+    value: String,
+    flags: i32
+}
+
+#[cfg(feature = "asm")]
+impl WinEntry {
+    pub fn new(text: &str, value: &str, flags: i32) -> WinEntry {
+        WinEntry {
+            text: String::from(text),
+            value: String::from(value),
+            flags: flags
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        self.value.as_str()
+    }
+}
+
+#[cfg(feature = "asm")]
+#[cfg(target_arch = "x86_64")]
+pub fn win_entries(title: &str, text: &str, suggested_width: i32,
+                   flex_down: i32, flex_up: i32, data_width: i32,
+                   entries: &mut [WinEntry], buttons: &[&str]) -> i32 {
+    let mut rv: i32;
+
+    let title = CString::new(title).unwrap();
+    let text  = CString::new(text).unwrap();
+
+    let buttons = str_slice_to_cstring_vec(buttons);
+    let mut button_ptrs = cstring_vec_to_ptrs(&buttons);
+    button_ptrs.reverse();
+
+    let entries_buf: *mut newtWinEntry;
+    let mut entries_text: Vec<CString> = Vec::new();
+
+    let values_buf: *mut *mut c_char;
+    let mut values_text: Vec<CString> = Vec::new();
+
+    unsafe {
+        let size = size_of::<newtWinEntry>() * (entries.len() + 1);
+        entries_buf = libc::malloc(size) as *mut newtWinEntry;
+        libc::memset(entries_buf as *mut c_void, 0, size);
+
+        let size = size_of::<*mut c_char>() * (entries.len());
+        values_buf = libc::malloc(size) as *mut *mut c_char;
+        libc::memset(values_buf as *mut c_void, 0, size);
+
+        for (cnt, entry) in entries.iter().enumerate() {
+            let entry_buf = entries_buf.offset(cnt as isize);
+            let value_buf = values_buf.offset(cnt as isize);
+            let text = CString::new(entry.text.as_str()).unwrap();
+            let value = CString::new(entry.value.as_str()).unwrap();
+            *value_buf = value.as_ptr() as *mut i8;
+
+            (*entry_buf).text = text.as_ptr() as *mut i8;
+            (*entry_buf).value = value_buf;
+            (*entry_buf).flags = entry.flags;
+            entries_text.push(text);
+            values_text.push(value);
+        }
+
+        asm! {
+            "movq $9,     %rcx
+             movq $8,     %rsi
+             movq %rcx,   %rbx
+
+             test $$1,    %rcx
+             jnz win_entries_loop
+
+             subq $$8,    %rsp
+             addq $$1,    %rbx
+
+             win_entries_loop:
+             movq (%rsi), %rax
+             pushq        %rax
+             addq $$8,    %rsi
+             loop         win_entries_loop
+
+             movq $7,     %rax
+             pushq        %rax
+
+             movq $1,     %rdi
+             movq $2,     %rsi
+             mov  $3,     %rdx
+             mov  $4,     %rcx
+             mov  $5,     %r8
+             mov  $6,     %r9
+
+             movq $$0,    %rax
+             call newtWinEntries
+             mov  %eax,   $0
+
+             addq $$1,    %rbx
+             movq %rbx,   %rax
+             movq $$8,    %rbx
+             mulq %rbx
+             addq %rax,   %rsp"
+
+            : "=r"(rv)
+            : "m"(title.as_ptr()), "m"(text.as_ptr()), "m"(suggested_width),
+              "m"(flex_down), "m"(flex_up), "m"(data_width), "m"(entries_buf),
+              "m"(button_ptrs.as_ptr()),  "m"(button_ptrs.len())
+            : "rsp", "rax", "rbx", "rcx", "rdx", "rdi", "rsi", "r8", "r9"
+        }
+
+        for (cnt, entry) in entries.iter_mut().enumerate() {
+            let buf = entries_buf.offset(cnt as isize);
+            let value = CStr::from_ptr(*(*buf).value).to_str().unwrap();
+            entry.value = String::from(value);
+            libc::free(*(*buf).value as *mut c_void);
+        }
+
+        libc::free(entries_buf as *mut c_void);
+        libc::free(values_buf as *mut c_void);
+    }
+
+    return rv;
 }
